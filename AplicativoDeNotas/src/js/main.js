4496433e2cf3a1c75ed7c892cdde0db13de58e9b
Mr Jabes <jabessilva31@gmail.com>
2024-07-29 19:07:33 -0300

    ...

--- AplicativoDeNotas/src/js/main.js
+++ AplicativoDeNotas/src/js/main.js
@@ -5,7 +5,7 @@ document.querySelector('#app').innerHTML = `
       <div class = "barra__de__Divisao">
         <button class = "botao_Adicionar" type- "button">Adicionar Nota</button>
         <div class = "lista_notas">
-  
+            
         </div>
       </div>
       <div class =
