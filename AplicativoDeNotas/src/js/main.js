7cd8a5dc0b4d54c48c9d8f3f97a59a49b606d72b
Mr Jabes <jabessilva31@gmail.com>
2024-07-30 07:07:26 -0300

    parte  inicial das notas lado esquerdo

--- AplicativoDeNotas/src/js/main.js
+++ AplicativoDeNotas/src/js/main.js
@@ -4,10 +4,15 @@ document.querySelector('#app').innerHTML = `
   <div class = "notas">
       <div class = "barra__de__Divisao">
         <button class = "botao_Adicionar" type- "button">Adicionar Nota</button>
-        <div class = "lista_notas">
-            
-        </div>
+          <div class = "notas_lista">
+              <div class = "lista-notas-item">
+                <div class = "notas_pequenas-titulo">Titulo</div>
+                <div class = "notas_pequenas-corpo">Descricao</div>
+                <div class = "notas_pequenas-salvas">Data</div> 
+              </div>
+          </div>
       </div>
-      <div class =
+        <div class ="notas_visualizacao">
+        </div>
   </div>
 `
