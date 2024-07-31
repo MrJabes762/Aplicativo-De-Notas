3ec8a19dbeb9902cb71ef0d3dcace6030297d8d9
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2024-07-31 15:37:02 -0300

    Delete AplicativoDeNotas/src directory
    Ajuste de versionamento

--- AplicativoDeNotas/src/js/main.js
+++
@@ -1,9 +0,0 @@
-import NotasAPI from './NotasAPI.js'
-
-NotasAPI.salvaNotas ({
-  id:436868,// a criação de uma lista de notas ja usando a API
-  titulo:"Essa nota foi modificada",
-  corpo: "Eu sou uma nova nota",
-});
-
-console.log(NotasAPI.getTodasNotas());
