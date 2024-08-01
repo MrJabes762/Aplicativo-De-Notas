25a5419f97ccb277377ea2ad203d19a5ca7ec236
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2024-08-01 14:49:16 -0300

    Desenvolvimento (#7)
    * Add: Logica de adicionar a lista, exibir lista, ajustes visuais, correção de repositorio
    
    * Add: Função deletar Nota
    
    * Finalizando App: Ajuste das funcionalidades do App (adicionar,Deletar,visualizar, selecionar) e alguns comentarios

--- AplicativoDeNotas/src/js/NotasView.js
+++ AplicativoDeNotas/src/js/NotasView.js
@@ -1,11 +1,11 @@
 export default class NotasView {
-    constructor(root, { notaSelecionada, notaAdicionada, notaEditada, notaDeletada } = {}) {
+    constructor(root, { notaSelecionada, notaAdicionada, notaEditada, notaDeletada } = {}) {//root é o contexto o app
         this.root = root;
         this.notaSelecionada = notaSelecionada;
         this.notaAdicionada = notaAdicionada;
         this.notaEditada = notaEditada;
         this.notaDeletada = notaDeletada;
-
+        //aqui embaixo é a parte do HTML Direito 
         this.root.innerHTML = `
             <div class="barra__de__Divisao">
                 <button class="botao_Adicionar" type="button">Adicionar Nota</button>
@@ -21,8 +21,8 @@ export default class NotasView {
         const pegaTitulo = this.root.querySelector(".notas_titulo");
         const pegaDescricao = this.root.querySelector(".notas_corpo");
 
-        if (btnAddNota) {
-            btnAddNota.addEventListener("click", () => {
+        if (btnAddNota) {// verificação se o botão é nulo 
+            btnAddNota.addEventListener("click", () => {// quando clicado aciona o metodo de adicionar
                 if (this.notaAdicionada) {
                     this.notaAdicionada();
                 } else {
@@ -33,20 +33,22 @@ export default class NotasView {
             console.error("O botão Adicionar Nota não foi encontrado.");
         }
 
-        [pegaTitulo, pegaDescricao].forEach(pegaCampo => {
+        [pegaTitulo, pegaDescricao].forEach(pegaCampo => { // aqui é para a edição da nota fazendo um foreach em todas as notas para verificar qual editar
             pegaCampo.addEventListener("blur", () => {
                 const tituloAdicionado = pegaTitulo.value.trim();
                 const descricaoAdicionada = pegaDescricao.value.trim();
-                if (this.notaEditada) {
+                try {
                     this.notaEditada(tituloAdicionado, descricaoAdicionada);
-                } else {
-                    console.error("A função notaEditada não foi passada como parâmetro.");
+                } catch (error) {
+                    console.error(`A nota não foi editada ${error}`);
                 }
             });
         });
+
+        this.subirVisualNotaVsibilidade(false);//inicialmente a visibiidade e falsa 
     }
 
-    _criarListaDeItemsHTML(id, titulo, corpo, data) {
+    _criarListaDeItemsHTML(id, titulo, corpo, data) {// a view que representa a nota do lado esquerdo
         const TAMANHO_DO_CORPO = 60;
         return `
             <div class="notas_lista-item" data-nota-id="${id}">
@@ -56,33 +58,46 @@ export default class NotasView {
                     ${corpo.length > TAMANHO_DO_CORPO ? "..." : ""}
                 </div>
                 <div class="notas_pequenas-salvas">
-                    ${data.toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })}
+                    ${new Date(data).toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })}
                 </div>
             </div>
         `;
     }
 
-    subirNotasLista(notas) {//metodo q recebe a nota e e aDiciona nota ao fim da proxima
+    subirNotasLista(notas) {//subindo a lista de notas todas 
         const containerListaNotas = this.root.querySelector(".notas_lista");
         containerListaNotas.innerHTML = "";
         for (const nota of notas) {
-            const html = this._criarListaDeItemsHTML(nota.id, nota.titulo, nota.corpo || "", new Date(nota.data));
+            const html = this._criarListaDeItemsHTML(nota.id, nota.titulo, nota.corpo || "", nota.salva);
             containerListaNotas.insertAdjacentHTML("beforeend", html);
         }
 
-        containerListaNotas.querySelectorAll(".notas_lista-item").forEach(listaDeItensDeNotas => {// metodo para a nota selecionada pegando o ID
+        containerListaNotas.querySelectorAll(".notas_lista-item").forEach(listaDeItensDeNotas => {//Deletação visual de uma nota na lista com o Clique duplo 
             listaDeItensDeNotas.addEventListener("click", () => {
                 this.notaSelecionada(listaDeItensDeNotas.dataset.notaId);
             });
-            
-            listaDeItensDeNotas.addEventListener ("dblclick", () => {//essa função deleta aquela nota que clicar 2 vezes 
-                const doDelete = confirm ("Você quer mesmo Deletar a nota ?");
-                
+
+            listaDeItensDeNotas.addEventListener("dblclick", () => {
+                const doDelete = confirm("Você quer mesmo Deletar a nota?");
                 if (doDelete) {
                     this.notaDeletada(listaDeItensDeNotas.dataset.notaId);
                 }
             });
         });
-        
+    }
+
+    subirNotasAtivas(nota) {// subir as notas configuradas como ativas 
+        this.root.querySelector(".notas_titulo").value = nota.titulo;
+        this.root.querySelector(".notas_corpo").value = nota.corpo;
+
+        this.root.querySelectorAll(".notas_lista-item").forEach(listaDeItensDeNotas => {
+            listaDeItensDeNotas.classList.remove("notas_lista-item--selecionada");
+        });
+
+        this.root.querySelector(`.notas_lista-item[data-nota-id="${nota.id}"]`).classList.add("notas_lista-item--selecionada");
+    }
+
+    subirVisualNotaVsibilidade(visible) {// ajusta a visibilidade das notas 
+        this.root.querySelector(".notas_visualizacao").style.visibility = visible ? "visible" : "hidden";
     }
 }
--- AplicativoDeNotas/src/js/main.js
+++ AplicativoDeNotas/src/js/main.js
@@ -1,22 +1,4 @@
-import NotasView from './NotasView.js';
-import NotasAPI from './NotasAPI.js';
+import App from "./App.js";// tudo começa aqui
 
-const app = document.getElementById('app'); // Pegando o contexto do App/ documento 
-// Passando ele pra dentro do Objeto NotasView
-const view = new NotasView(app, {
-    notaSelecionada (id){
-        console.log ("Nota Selecionada", id)
-    },
-    notaAdicionada() {
-        console.log("Essa nota foi adicionada");
-    },
-    notaEditada(novoTitulo, novoCorpo) {
-        console.log(novoTitulo);
-        console.log(novoCorpo);
-    },
-    notaDeletada (id){
-        console.log ("Nota Deletada", id)
-    }
-});
-
-view.subirNotasLista(NotasAPI.getTodasNotas());
+const root = document.getElementById('app'); // Pegando o contexto do App/ documento 
+const app = new App(root);//enviando para dentro do App
